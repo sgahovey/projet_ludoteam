315acc39b6020c0d27986bf88f954623ca4e3867
AHOVEY Saint-George <sgahovey@gmail.com>
2025-01-17 11:37:05 +0400

    Configuration de l'affichage total des jeux>

--- src/Controller/JeuController.php
+++ src/Controller/JeuController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\JeuRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class JeuController extends AbstractController
 {
     #[Route('/jeu', name: 'app_jeu')]
-    public function index(): Response
+    public function index(JeuRepository $jeuRepository): Response
     {
+        // Récupérer tous les jeux depuis la base de données
+        $jeux = $jeuRepository->findAll();
+
         return $this->render('jeu/index.html.twig', [
-            'controller_name' => 'JeuController',
+            'jeux' => $jeux,
         ]);
     }
 }
