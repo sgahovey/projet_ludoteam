75b2ad80c01dbb4ea1908d981a91674040dba7d1
AHOVEY Saint-George <sgahovey@gmail.com>
2025-01-17 10:27:17 +0400

    Suppression de l'attribut id  dans l'entité <JeuCarte> car erreur lors de la migration

--- src/Entity/JeuCarte.php
+++ src/Entity/JeuCarte.php
@@ -9,21 +9,11 @@ use Doctrine\ORM\Mapping as ORM;
 class JeuCarte extends Jeu
 
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
     #[ORM\Column(length: 255)]
     private ?string $format = null;
 
     #[ORM\Column]
     private ?bool $joker_inclus = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getFormat(): ?string
     {
