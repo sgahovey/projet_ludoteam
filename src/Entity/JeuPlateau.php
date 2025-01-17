7baef985f5ff63d7e42a631f9d22a3e4989e5d7f
AHOVEY Saint-George <sgahovey@gmail.com>
2025-01-17 09:45:44 +0400

    Création de l'éntité  <JeuPlateau>

---
+++ src/Entity/JeuPlateau.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JeuPlateauRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JeuPlateauRepository::class)]
+class JeuPlateau
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $dimension_plateau = null;
+
+    #[ORM\Column]
+    private ?int $nb_case = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDimensionPlateau(): ?string
+    {
+        return $this->dimension_plateau;
+    }
+
+    public function setDimensionPlateau(string $dimension_plateau): static
+    {
+        $this->dimension_plateau = $dimension_plateau;
+
+        return $this;
+    }
+
+    public function getNbCase(): ?int
+    {
+        return $this->nb_case;
+    }
+
+    public function setNbCase(int $nb_case): static
+    {
+        $this->nb_case = $nb_case;
+
+        return $this;
+    }
+}
