31199de39458e6f8eca314aa3786110af4a8d679
AHOVEY Saint-George <sgahovey@gmail.com>
2025-01-17 10:27:55 +0400

    Suppression de l'attribut id  dans l'entité <JeuDuel> car erreur lors de la migration

--- src/Entity/JeuDuel.php
+++ src/Entity/JeuDuel.php
@@ -9,21 +9,11 @@ use Doctrine\ORM\Mapping as ORM;
 class JeuDuel extends Jeu
 
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
     #[ORM\Column]
     private ?int $dureeMax = null;
 
     #[ORM\Column(length: 255)]
     private ?string $type_adversaire = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getDureeMax(): ?int
     {
