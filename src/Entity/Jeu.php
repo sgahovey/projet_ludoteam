842397dfc951980e5dadb4c9aa519745fabd53bd
AHOVEY Saint-George <sgahovey@gmail.com>
2025-01-17 09:36:30 +0400

    Création de l'éntité  <Jeu>

---
+++ src/Entity/Jeu.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JeuRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: JeuRepository::class)]
+class Jeu
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $nb_participant = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNbParticipant(): ?int
+    {
+        return $this->nb_participant;
+    }
+
+    public function setNbParticipant(int $nb_participant): static
+    {
+        $this->nb_participant = $nb_participant;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
