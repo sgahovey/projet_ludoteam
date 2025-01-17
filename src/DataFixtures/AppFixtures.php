4502dad10b067ecf53f9a15cb28db173cefc5533
AHOVEY Saint-George <sgahovey@gmail.com>
2025-01-17 11:08:57 +0400

    Ajout des factorys dans l'appfixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,26 +2,25 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Jeu;
-use App\Entity\JeuCarte;
-use App\Entity\JeuDuel;
-use App\Entity\JeuPlateau;
-
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Factory\JeuDuelFactory;
+use App\Factory\JeuCarteFactory;
+use App\Factory\JeuPlateauFactory;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-     // Création de jeux de cartes
-     for ($i = 0; $i < 5; $i++) {
-        $jeuCarte = new JeuCarte();
-        $jeuCarte->setNom('Jeu de Cartes ' . $i);
-        $jeuCarte->setNbParticipant(rand(2, 10));
-        $jeuCarte->setFormat('Tarot');
-        $jeuCarte->setJokerInclus((bool) rand(0, 1));
-        $manager->persist($jeuCarte);
-    }
+        // Générer des jeux de cartes
+        JeuCarteFactory::new()->createMany(5);
+
+        // Générer des jeux de duel
+        JeuDuelFactory::new()->createMany(5);
+
+        // Générer des jeux de plateau
+        JeuPlateauFactory::new()->createMany(5);
+
+        $manager->flush();
     }
 }
