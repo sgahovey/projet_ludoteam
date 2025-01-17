f976bcdd54e8815962e999a46c8e57cd06d2730a
AHOVEY Saint-George <sgahovey@gmail.com>
2025-01-17 10:49:20 +0400

    Test de l'ajout des datas fixtures pour les jeux de cartes

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Jeu;
+use App\Entity\JeuCarte;
+use App\Entity\JeuDuel;
+use App\Entity\JeuPlateau;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +14,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+     // Création de jeux de cartes
+     for ($i = 0; $i < 5; $i++) {
+        $jeuCarte = new JeuCarte();
+        $jeuCarte->setNom('Jeu de Cartes ' . $i);
+        $jeuCarte->setNbParticipant(rand(2, 10));
+        $jeuCarte->setFormat('Tarot');
+        $jeuCarte->setJokerInclus((bool) rand(0, 1));
+        $manager->persist($jeuCarte);
+    }
     }
 }
